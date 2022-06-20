49c5708c33fa6b324ca16d335f334df8168862ba
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-20 10:54:10 +0000

    Mon, Jun 20, 2022, 10:54 AM +00:00

--- compare.c
+++ compare.c
@@ -9,5 +9,15 @@ int main(void)
     char *s = get_string("s: ");
     char *t = malloc(strlen(s) + 1);
 
-    t[0] = toupper;
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        t[i] = s[i];
+    }
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+    free(t);
 }

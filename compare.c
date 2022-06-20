456bc24261e678459b4b03ef16a899b750d3b581
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-20 10:40:50 +0000

    Mon, Jun 20, 2022, 10:40 AM +00:00

--- compare.c
+++ compare.c
@@ -2,11 +2,12 @@
 #include <string.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
-    char *t = s;
+    char *t = malloc(strlen(s) + 1);
 
     t[0] = toupper;
 }

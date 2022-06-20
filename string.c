ffbcb19a3206927d061bfd8e02b993333b4e18f1
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-20 09:24:06 +0000

    Mon, Jun 20, 2022, 9:24 AM +00:00

--- string.c
+++ string.c
@@ -5,9 +5,6 @@
 int main(void)
 {
     char *s = "HI!";
-    for (int i = 0; i < strlen(s); i++)
-    {
-        printf("%c", s[i]);
-    }
-    printf("\n");
+    printf("%p\n", s);
+    printf("%c\n", s)
 }

98685798076a7165bcb68eaae346a86206d18efc
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-20 10:07:33 +0000

    Mon, Jun 20, 2022, 10:07 AM +00:00

--- numbers.c
+++ numbers.c
@@ -3,6 +3,9 @@
 int main(void)
 {
     int numbers[] = {4, 6, 8, 2, 7, 5, 0};
-    for(int i = 0 )
-    printf("%i\n", *numbers);
+    int size = sizeof numbers / sizeof numbers[0];
+    for(int i = 0; i < size; i++)
+    {
+        printf("%i\n", *(numbers + i));
+    }
 }

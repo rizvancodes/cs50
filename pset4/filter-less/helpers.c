ee24a42b4f9aaaf24ca34fea0b59fa9ad79638d9
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-30 14:51:59 +0000

    Thu, Jun 30, 2022, 2:51 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -29,6 +29,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue
             float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue
             int avg = round(x);
+            if (round(sepiaRed > 255))
+            {
+                
+            }
             image[i][j].rgbtRed = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtBlue = avg;

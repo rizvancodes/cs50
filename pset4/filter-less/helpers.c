b9b7f3798afa8f5ef0db6baaee1b2a780c4a7b97
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-30 14:40:30 +0000

    Thu, Jun 30, 2022, 2:40 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -8,8 +8,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float x = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue / 3.0;
-            avg = round(x);
+            float x = ((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            int avg = round(x);
             image[i][j].rgbtRed = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtBlue = avg;

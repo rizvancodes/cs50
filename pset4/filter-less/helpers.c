8d7ef7e72d4c922c13afbcb50f144bb93ea03dfa
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-30 14:02:50 +0000

    Thu, Jun 30, 2022, 2:02 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int x = image[i][j].rgbtRed;
-            printf();
+            printf(%i, x);
         }
     }
     return;

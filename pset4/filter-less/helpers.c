5362a015d3e27720921527daae2a94201d5e9e4e
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-30 13:08:08 +0000

    Thu, Jun 30, 2022, 1:08 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -3,6 +3,10 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (i = 0; i < height; i++)
+    {
+        for (j)
+    }
     return;
 }
 

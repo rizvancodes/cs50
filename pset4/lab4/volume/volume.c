dfb0537d89475514e8e7dadc89b7366af3dd427b
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-30 08:41:39 +0000

    Thu, Jun 30, 2022, 8:41 AM +00:00

--- pset4/lab4/volume/volume.c
+++ pset4/lab4/volume/volume.c
@@ -41,9 +41,8 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     char ch;
     int16_t buffer, temp;
-    while((ch = fgetc(input)) != EOF )
+    while(fread(&buffer, sizeof(int16_t), 1, input))
     {
-        fread(&buffer, sizeof(int16_t), 1, input);
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }

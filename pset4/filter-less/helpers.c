3879f749d4bb51cf0e0fe727bb26eeb88395780b
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-06-30 14:54:53 +0000

    Thu, Jun 30, 2022, 2:54 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -25,17 +25,33 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue
-            float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue
-            float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue
-            int avg = round(x);
-            if (round(sepiaRed > 255))
+            float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            if ((round(sepiaRed) > 255))
             {
-                
+                image[i][j].rgbtRed = round(sepiaRed) = 255;
             }
-            image[i][j].rgbtRed = avg;
-            image[i][j].rgbtGreen = avg;
-            image[i][j].rgbtBlue = avg;
+            else
+            {
+                image[i][j].rgbtRed = round(sepiaRed);
+            }
+            if ((round(sepiaGreen) > 255))
+            {
+                image[i][j].rgbtGreen = round(sepiaGreen) = 255;
+            }
+            else
+            {
+                image[i][j].rgbtGreen = round(sepiaGreen);
+            }
+            if ((round(sepiaBlue) > 255))
+            {
+                image[i][j].rgbtBlue = round(sepiaBlue) = 255;
+            }
+            else
+            {
+                image[i][j].rgbtBlue = round(sepiaBlue);
+            }
         }
     }
     return;

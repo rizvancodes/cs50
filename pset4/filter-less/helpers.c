671225a20129f5d69c8ee320e96915f9fd99faaf
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-01 08:45:51 +0000

    Fri, Jul 1, 2022, 8:45 AM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -90,29 +90,35 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < round(width / 2.0); j++)
         {
-            if (i = 0 && j = 0)
+            if (i == 0 && j == 0)
             {
                 image[i][j].rgbtRed = round(copy[i][j].rgbtred + copy[i + 1][j].rgbtred + copy[i + 1][j + 1].rgbtred + copy[i][j +1 ].rgbtred / 4.0);
                 image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i + 1][j + 1].rgbtGreen + copy[i][j + 1].rgbtGreen / 4.0);
                 image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i + 1][j + 1].rgbtBlue + copy[i][j + 1].rgbtBlue / 4.0);
             }
-            else if (i = 0 && j = (width - 1))
+            else if (i == 0 && j == (width - 1))
             {
                 image[i][j].rgbtRed = round(copy[i][j].rgbtred + copy[i + 1][j].rgbtred + copy[i + 1][j - 1].rgbtred + copy[i][j - 1].rgbtred / 4.0);
                 image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i + 1][j - 1].rgbtGreen + copy[i][j - 1].rgbtGreen / 4.0);
                 image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i + 1][j - 1].rgbtBlue + copy[i][j - 1].rgbtBlue / 4.0);
             }
-            else if (i = (height - 1) && j = 0)
+            else if (i == (height - 1) && j == 0)
             {
                 image[i][j].rgbtRed = round(copy[i][j].rgbtred + copy[i - 1][j].rgbtred + copy[i - 1][j + 1].rgbtred + copy[i][j + 1].rgbtred / 4.0);
                 image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i - 1][j + 1].rgbtGreen + copy[i][j + 1].rgbtGreen / 4.0);
                 image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i - 1][j + 1].rgbtBlue + copy[i][j + 1].rgbtBlue / 4.0);
             }
-            else if (i = (height - 1) && j = (width - 1))
+            else if (i == (height - 1) && j == (width - 1))
             {
-                image[i][j].rgbtRed = round(copy[i][j].rgbtred + copy[i - 1][j].rgbtred + copy[i - 1][j + 1].rgbtred + copy[i][j + 1].rgbtred / 4.0);
-                image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i - 1][j + 1].rgbtGreen + copy[i][j + 1].rgbtGreen / 4.0);
-                image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i - 1][j + 1].rgbtBlue + copy[i][j + 1].rgbtBlue / 4.0);
+                image[i][j].rgbtRed = round(copy[i][j].rgbtred + copy[i - 1][j].rgbtred + copy[i - 1][j - 1].rgbtred + copy[i][j - 1].rgbtred / 4.0);
+                image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i - 1][j - 1].rgbtGreen + copy[i][j - 1].rgbtGreen / 4.0);
+                image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i - 1][j - 1].rgbtBlue + copy[i][j - 1].rgbtBlue / 4.0);
+            }
+            else if (i > 0 && j == (width - 1))
+            {
+                image[i][j].rgbtRed = round(copy[i][j].rgbtred + copy[i - 1][j].rgbtred + copy[i - 1][j - 1].rgbtred + copy[i][j - 1].rgbtred / 4.0);
+                image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i - 1][j - 1].rgbtGreen + copy[i][j - 1].rgbtGreen / 4.0);
+                image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i - 1][j - 1].rgbtBlue + copy[i][j - 1].rgbtBlue / 4.0);
             }
         }
     }

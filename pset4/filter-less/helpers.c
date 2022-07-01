24091be32b69f3ed33cb43cdc75b91654e2b08cb
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-01 07:52:41 +0000

    Fri, Jul 1, 2022, 7:52 AM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -62,7 +62,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < round(width / 2.0); j++)
         {
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][(width - 1) - j];

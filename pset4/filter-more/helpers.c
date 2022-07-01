fabb1b8e86507d0f85b3a86c8b59091aad864425
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-01 14:34:20 +0000

    Fri, Jul 1, 2022, 2:34 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -36,21 +36,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < round(width / 2.0); j++)
-        {
-            RGBTRIPLE temp = image[i][j];
-            image[i][j] = image[i][(width - 1) - j];
-            image[i][(width - 1) - j] = temp;
-        }
-    }
-    return;
-}
-
-// Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
-{
     //create a copy of the array
     RGBTRIPLE copy[height][width];
     for (int i = 0; i < height; i++)

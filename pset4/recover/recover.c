f93e282e77c3d80b784d1ea208cc0b85328194cc
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-04 08:26:03 +0000

    Mon, Jul 4, 2022, 8:26 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ //check for invalid usage
+ if (argc < 2 || argc > 2)
+ {
+    printf("Usage: ./recover filename.raw\n")
+    return 1;
+ }
 }

11db879884c6955220b845b67348d8392619cce6
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-04 09:57:24 +0000

    Mon, Jul 4, 2022, 9:57 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -22,7 +22,8 @@ FILE *f = fopen(argv[1], "r");
 
 //declare new buffer
 BYTE buffer[malloc(BLOCK_SIZE)];
-
+//number of jpegs
+int jpegnum = 0
 //begin reading from new file as long as fread returns a value of 512 bytes read
 while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, argv[1]) == BLOCK_SIZE)
 {
@@ -32,6 +33,7 @@ fread(buffer, sizeof(BYTE), BLOCK_SIZE, argv[1]);
 //look for beginning of jpeg
 if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
 {
+   jpegnum++
    FILE *f = fopen()
 }
 }

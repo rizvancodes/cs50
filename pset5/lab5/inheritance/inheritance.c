3da498382f32b4eba3ccc9790d482e6c58bdc03c
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-06 14:49:55 +0000

    Wed, Jul 6, 2022, 2:49 PM +00:00

--- pset5/lab5/inheritance/inheritance.c
+++ pset5/lab5/inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *child = malloc(sizeof(person));
+    person *p = malloc(sizeof(person));
     if (n == NULL)
     {
         free_family(person);
@@ -54,22 +54,22 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        child->parents[0] = *parent0;
-        child->parents[1] = *parent1;
+        p->parents[0] = *parent0;
+        p->parents[1] = *parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        child->alleles[0] = parent0->alleles[rand() % 2];
-        child->alleles[1] = parent1->alleles[rand() % 2];
+        p->alleles[0] = parent0->alleles[rand() % 2];
+        p->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        child->parents[0] = NULL;
-        child->parents[1] = NULL;
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
         // TODO: Randomly assign alleles
-        child->alleles[0] = random_alleles();
-        child->alleles[1] = random_alleles();
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person

e58037bfc322a45d609a4b598c4e778a21c16168
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-06 14:34:19 +0000

    Wed, Jul 6, 2022, 2:34 PM +00:00

--- pset5/lab5/inheritance/inheritance.c
+++ pset5/lab5/inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *child = malloc(sizeof(person));
+    person *family = malloc(sizeof(person));
     if (n == NULL)
     {
         free_family(person);
@@ -57,7 +57,8 @@ person *create_family(int generations)
         child->parents[0] = *parent0;
         child->parents[1] = *parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        
+        child->alleles[0] = parent0->alleles[rand() % 2];
+        child->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create

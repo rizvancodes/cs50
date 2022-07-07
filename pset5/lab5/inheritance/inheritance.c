4148ad5e74046886a75fc2b4185096503553fd2f
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-07 07:50:53 +0000

    Thu, Jul 7, 2022, 7:50 AM +00:00

--- pset5/lab5/inheritance/inheritance.c
+++ pset5/lab5/inheritance/inheritance.c
@@ -88,7 +88,8 @@ void free_family(person *p)
     // TODO: Free parents recursively
     else
     {
-        
+        free_family(p->parents[0]);
+        free_family(p->parents[1]);
     }
     // TODO: Free child
 

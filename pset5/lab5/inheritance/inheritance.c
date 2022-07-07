013cd118405f509c086c0a4f81070612ff8e6b6c
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-07-07 08:07:21 +0000

    Thu, Jul 7, 2022, 8:07 AM +00:00

--- pset5/lab5/inheritance/inheritance.c
+++ pset5/lab5/inheritance/inheritance.c
@@ -54,8 +54,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        parents[0] = *parent0;
-        parents[1] = *parent1;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         p->alleles[0] = parent0->alleles[rand() % 2];
         p->alleles[1] = parent1->alleles[rand() % 2];

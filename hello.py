a5b21874489ebcd85516a68b1e3a3d0ce35b602f
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-16 07:20:47 +0000

    ddd, Oct 10/16/2022, YYYY, 7:20 A +0

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 
-
-x = int(input("Type your first number "))
-y = int(input("Type your second number "))
+try:
+    x = int(input("Type your first number "))
+except:
+        print("That is not an int!")
+        exit()
+try:
+    y = int(input("Type your second number "))
+except:
+    print("That is not an int!")
+    exit()
 print(x + y)

be3b49a575b0b6db60ff6ef923cb81e1f22cf31e
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-16 07:17:34 +0000

    ddd, Oct 10/16/2022, YYYY, 7:17 A +0

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-import cs50
 
-x = cs50.get_int("Type your first number ")
-y = cs50.get_int("Type your second number ")
+
+x = input("Type your first number ")
+y = input("Type your second number ")
 print(x + y)

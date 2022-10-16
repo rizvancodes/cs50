630eabd10064fe006e3270de3aa61c8176507f5e
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-16 07:45:58 +0000

    ddd, Oct 10/16/2022, YYYY, 7:45 A +0

--- mario.py
+++ mario.py
@@ -1,17 +1,17 @@
 def main():
     height = get_height()
     for i in range(height):
-        print("#")
+        print("#", " ")
 
 def get_height():
     while True:
         try:
             x = int(input("Size: "))
-            if n > 0:
+            if x > 0:
                 break
         except ValueError:
                 print("That is not an int!")
                 exit()
-    return n
+    return x
 
 main()

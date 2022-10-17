af5ea78fe1ebeffc8418e6ea83aba33b5bf83091
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-17 19:31:14 +0000

    ddd, Oct 10/17/2022, YYYY, 7:31 A +0

--- pset6/lab6/world-cup/tournament.py
+++ pset6/lab6/world-cup/tournament.py
@@ -20,7 +20,7 @@ def main():
     with open(wcfile) as file:
         reader = csv.DictReader(file)
         for row in reader
-        
+            
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 

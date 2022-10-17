0329efadb0b625285879773090627233724d4111
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-17 21:17:21 +0000

    ddd, Oct 10/17/2022, YYYY, 9:17 A +0

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -4,7 +4,13 @@ def main():
     while height < 1 or height > 8:
         height = int(input("Type an integer between 1 and 8 "))
 
-    
+    for i in range(height):
+        for i in range(height - 1):
+            print(" ")
+        for i in range(i):
+            print("#  ")
+        for i in range(i):
+            print("#")
 
 if __name__ == "__main__":
     main()

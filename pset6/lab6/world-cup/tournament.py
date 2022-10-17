f8c853a023f8f457852357de0859544fe2467bc7
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-17 19:54:31 +0000

    ddd, Oct 10/17/2022, YYYY, 7:54 A +0

--- pset6/lab6/world-cup/tournament.py
+++ pset6/lab6/world-cup/tournament.py
@@ -25,7 +25,7 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    
+
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
@@ -55,7 +55,7 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    twinners = 
 
 
 if __name__ == "__main__":

f989ba71529e45e010e9694f76af7ed08183b975
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-17 22:29:36 +0000

    ddd, Oct 10/17/2022, YYYY, 10:29 A +0

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -5,9 +5,11 @@ def main():
         height = int(input("Type an integer between 1 and 8 "))
 
     for i in range(height):
-        print(i)
-        for j in range(height - 1):
-            print(j)
+        for j in range(height - (i + 1)):
+            print(" ", end="")
+        for k in range(i + 1):
+            print("#", end="")
+        
 
 
 

9f542a4e8db46a249a14e6a72d0e848679247e5b
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-18 22:59:21 +0000

    ddd, Oct 10/18/2022, YYYY, 10:59 A +0

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -5,7 +5,8 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python dna.py FILENAME")
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable

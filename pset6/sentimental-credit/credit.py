635088087e00c4a77370e373ea39acc810070ac8
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-18 19:41:28 +0000

    ddd, Oct 10/18/2022, YYYY, 7:41 A +0

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -1,11 +1,14 @@
 # TODO
 #get card number as string, cast as list
-cardNum = list(input("Type your card number: "))
-print(cardNum)
-odd = cardNum[-2::-2]
-even = cardNum[-1::-2]
-checksum = 0
+def main()
+    cardNum = list(input("Type your card number: "))
+    print(cardNum)
+    odd = cardNum[-2::-2]
+    even = cardNum[-1::-2]
+    checksum = 0
 
+def get_digits(x)
+    for 
 #get alternate digits starting at -2, multiply by 2 and store as string
 #take each digit, cast as int and sum
 #get alternate digits starting at -1, cast as int and sum

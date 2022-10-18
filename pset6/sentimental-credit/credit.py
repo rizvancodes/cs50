ba3e379442de7606c2b8256eb8a029f42ad7b8fc
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-18 10:11:58 +0000

    ddd, Oct 10/18/2022, YYYY, 10:11 A +0

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -2,9 +2,11 @@
 #get card number as string, cast as list
 cardNum = list(input("Type your card number: "))
 print(cardNum)
+newList = ()
 for i in cardNum[::2]:
-    newList = [str(int(i) * 2).split for i in newList]
-print(newList)
+    newList = [str(int(i) * 2).split[0] for i in newList]
+    print(newList)
+
 #get alternate digits starting at -2, multiply by 2 and store as string
 #take each digit, cast as int and sum
 #get alternate digits starting at -1, cast as int and sum

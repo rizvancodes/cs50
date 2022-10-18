38c9e71aa7edf40d8428d8a2c65f00a3c09fd8b6
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-18 08:11:14 +0000

    ddd, Oct 10/18/2022, YYYY, 8:11 A +0

--- pset6/sentimental-readability/readability.py
+++ pset6/sentimental-readability/readability.py
@@ -3,7 +3,13 @@ sampleString = input("Enter your text: ")
 alpha = 0
 words = 0
 sentences = 0
+
 for i in sampleString:
     if(i.isalpha()):
         alpha += 1
+
+wordList = sampleString.split()
+words = len(wordList)
+
 print(alpha)
+print(words)

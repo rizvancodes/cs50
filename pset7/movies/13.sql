a1ebaf43e3132a9e9e6659e778717d46bcbb7b59
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 16:19:32 +0000

    ddd, Oct 10/21/2022, YYYY, 4:19 A +0

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,7 +1,5 @@
-SELECT people.name
-  FROM people
-  JOIN people ON stars.person_id = people.id
-  JOIN stars ON stars.movie_id = movies.id
- WHERE people.name IN ("Johnny Depp", "Helena Bonham Carter")
- GROUP BY movies.title
-HAVING count(people.name) = 2;
+SELECT DISTINCT people.name
+FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN people ON stars.person_id = people.id
+WHERE stars.movies_id IN (SELECT movie_id FROM stars WHERE person_id IN(SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958));

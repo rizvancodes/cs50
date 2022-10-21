e8ebe43c3d800371d9dfe646ee5295a39fd2357c
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 19:06:37 +0000

    ddd, Oct 10/21/2022, YYYY, 7:06 A +0

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
---s
+-- check tables structure
+.schema
+-- search description of crime scene
+SELECT description, street FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28;
+-- check interview schema
+.schema interviews

6fd9796f0b26e0ef353ee1ca62d5f0fab20e6f59
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 11:30:01 +0000

    ddd, Oct 10/21/2022, YYYY, 11:30 A +0

--- pset7/movies/9.sql
+++ pset7/movies/9.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT people.name FROM people JJOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2004 ORDER BY people.birth;
+SELECT DISTINCT people.name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2004 ORDER BY people.birth;

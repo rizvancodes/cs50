de2f7d678c7aef16fb4359d3bb81499d56ae862a
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 13:33:08 +0000

    ddd, Oct 10/21/2022, YYYY, 1:33 A +0

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -2,6 +2,6 @@ SELECT movies.title
   FROM movies
   JOIN stars ON stars.movie_id = movies.id
   JOIN people ON stars.person_id = people.id
- WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
+ WHERE people.name IN ("Johnny Depp", "Helena Bonham Carter")
 
- SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON stars.person_id = people.id WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
+ SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON stars.person_id = people.id WHERE people.name IN ("Johnny Depp", "Helena Bonham Carter");

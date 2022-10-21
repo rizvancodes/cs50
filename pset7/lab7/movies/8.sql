a5cf656b2b4751c17677a7be73671fe0ff1d672d
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 09:14:46 +0000

    ddd, Oct 10/21/2022, YYYY, 9:14 A +0

--- pset7/lab7/movies/8.sql
+++ pset7/lab7/movies/8.sql
@@ -1,1 +1,1 @@
-SELECT people.name FROM people JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year = 2010 ORDER BY ratings.rating;
+SELECT people.name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = "Toy Story";

c7f9432a32f758f4464a9592de4f79e3b954fadb
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 13:13:49 +0000

    ddd, Oct 10/21/2022, YYYY, 1:13 A +0

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -1,1 +1,6 @@
-SELECT DISTINCT people.name FROM people JOIN directors ON people.id = directors.person_id JOIN movies ON directors.movie_id = movies.id JOIN ratings ON ratings.movie_id = movies.id WHERE ratings.rating >= 9.0;
+SELECT DISTINCT people.name
+           FROM people
+           JOIN directors ON people.id = directors.person_id
+           JOIN movies ON directors.movie_id = movies.id
+           JOIN ratings ON ratings.movie_id = movies.id
+          WHERE ratings.rating >= 9.0;
--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,1 +1,8 @@
-SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON stars.person_id = people.id JOIN ratings ON ratings.movie_id = movies.id WHERE people.name = "Chadwick Boseman" ORDER BY ratings.rating DESC LIMIT 5;
+SELECT movies.title
+  FROM movies
+  JOIN stars ON stars.movie_id = movies.id
+  JOIN people ON stars.person_id = people.id
+  JOIN ratings ON ratings.movie_id = movies.id
+ WHERE people.name = "Chadwick Boseman"
+ ORDER BY ratings.rating DESC
+ LIMIT 5;

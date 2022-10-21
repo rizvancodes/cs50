224e5c6facbeb12cf81dac57176437731f8a25a6
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 13:49:29 +0000

    ddd, Oct 10/21/2022, YYYY, 1:49 A +0

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -5,5 +5,3 @@ SELECT movies.title
  WHERE people.name IN ("Johnny Depp", "Helena Bonham Carter")
  GROUP BY movies.title
 HAVING count(people.name) = 2;
-
- SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON stars.person_id = people.id WHERE people.name IN ("Johnny Depp", "Helena Bonham Carter") GROUP BY movies.title HAVING count(people.name) = 2;

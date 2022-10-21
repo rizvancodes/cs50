314efbf297d899468cb791e466de70c3dedf1bd5
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 11:47:45 +0000

    ddd, Oct 10/21/2022, YYYY, 11:47 A +0

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT people.name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings ON ratings.movie_id = movies.id WHERE 
+SELECT DISTINCT movies.title FROM movies JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings ON ratings.movie_id = movies.id WHERE

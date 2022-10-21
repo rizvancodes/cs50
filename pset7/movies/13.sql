a02552105977a33ddafe306ff13e9d5340e9fec6
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 16:21:32 +0000

    ddd, Oct 10/21/2022, YYYY, 4:21 A +0

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,5 +1,4 @@
 SELECT DISTINCT people.name
 FROM people
 JOIN stars ON people.id = stars.person_id
-JOIN people ON stars.person_id = people.id
 WHERE stars.movies_id IN (SELECT movie_id FROM stars WHERE person_id IN(SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958));

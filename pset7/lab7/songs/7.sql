c2efec155337f7642129f8e62fd4e0cb530d862a
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 08:29:09 +0000

    ddd, Oct 10/21/2022, YYYY, 8:29 A +0

--- pset7/lab7/songs/7.sql
+++ pset7/lab7/songs/7.sql
@@ -1,1 +1,1 @@
-SELECT songs.energy FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Drake";
+SELECT AVG(songs.energy) FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Drake";

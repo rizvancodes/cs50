dfc37eccf8cb3a032a70314c7513321abb8a6d98
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 13:56:38 +0000

    ddd, Oct 10/21/2022, YYYY, 1:56 A +0

--- pset7/movies/4.sql
+++ pset7/movies/4.sql
@@ -1,1 +1,1 @@
-SELECT movies.title FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating = 10.0;
+SELECT COUNT(movies.title) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating = 10.0;

0ac09d7daa82614c5c60181fda5c71a57a1b847d
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-21 17:07:10 +0000

    ddd, Oct 10/21/2022, YYYY, 5:07 A +0

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,1 +1,1 @@
-SELECT people.name FROM people WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958));
+SELECT name FROM people WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958));

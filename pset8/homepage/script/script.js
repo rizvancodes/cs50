c696983a7a437411736b007d04e57d542289e655
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-22 15:44:15 +0000

    ddd, Oct 10/22/2022, YYYY, 3:44 A +0

--- pset8/homepage/script/script.js
+++ pset8/homepage/script/script.js
@@ -1,14 +1,12 @@
-window.addEventListener("load", function () {
-    
+
 var i = 0;
 var txt = "Hi, I'm Rizvan"; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
+window.addEventListener("DOMContentLoaded", function () {
   if (i < txt.length) {
     document.getElementById("#typed").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
-}
 });

553619c495b74ed43fdc61ec5200b82e0a851bf3
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-22 15:40:09 +0000

    ddd, Oct 10/22/2022, YYYY, 3:40 A +0

--- pset8/homepage/script/script.js
+++ pset8/homepage/script/script.js
@@ -1,10 +1,11 @@
+
 var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
+var txt = "Hi, I'm Rizvan"; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("#typed").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }

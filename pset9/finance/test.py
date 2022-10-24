31b664e97935bedcd039e41616c3e8a7217892e3
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-24 19:31:58 +0000

    ddd, Oct 10/24/2022, YYYY, 7:31 A +0

--- pset9/finance/test.py
+++ pset9/finance/test.py
@@ -1,6 +1,6 @@
-username1 = {username: john}
-username2 = {username: peter}
-username3 = {username: harry}
-usernames[username1, username2, username3]
+username1 = {'username': 'john'}
+username2 = {'username': 'peter'}
+username3 = {'username': 'harry'}
+usernames = [username1, username2, username3]
 for username in usernames:
     print(username)

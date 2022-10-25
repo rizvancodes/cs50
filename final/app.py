af6278f712f2e6f4ed5e87bbe1b40de5980e69ca
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-25 19:40:30 +0000

    ddd, Oct 10/25/2022, YYYY, 7:40 A +0

--- final/app.py
+++ final/app.py
@@ -11,5 +11,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
+
+@app.route("/extcert")
+def extcert():
+    return render_template("extcert.html")

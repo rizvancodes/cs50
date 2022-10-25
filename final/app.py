c2704f133a5c4046fd1ae5ccaa9f771cccf5b847
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-25 23:55:08 +0000

    ddd, Oct 10/25/2022, YYYY, 11:55 A +0

--- final/app.py
+++ final/app.py
@@ -15,25 +15,21 @@ def index():
     initialize drop down menus
     """
 
-    units = {Applied Science: [Principles and Applications of Science I, Practical Scientific Procedures and Techniques]}
+    units = {"Applied Science": ["Principles and Applications of Science I", "Practical Scientific Procedures and Techniques"], "Health and Social Care": ["Human Lifespan Development", "Working in Health and Social Care]}
 
-    default_classes = sorted(class_entry_relations.keys())
-    default_values = class_entry_relations[default_classes[0]]
+    default_courses = sorted(units.keys())
+    default_units = units[default_courses[0]]
 
     return render_template('index.html',
-                       all_classes=default_classes,
-                       all_entries=default_values)
-    return render_template("index.html")
+                       all_courses=default_courses,
+                       all_units=default_units)
 
-@app.route("/extcert")
-def extcert():
-    return render_template("extcert.html")
 
 @app.route('/_update_dropdown')
 def update_dropdown():
 
     # the value of the first dropdown (selected by the user)
-    selected_course = request.args.get('selected_class', type=str)
+    selected_course = request.args.get('selected_course', type=str)
 
     # get values for the second dropdown
     updated_values = get_dropdown_values()[selected_class]

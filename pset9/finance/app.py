44b1b1a0a6ae839adea2626f055c44c9efb5974c
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-25 09:50:06 +0000

    ddd, Oct 10/25/2022, YYYY, 9:50 A +0

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -203,16 +203,16 @@ def sell():
         else:
             for stock in portfolio:
                 if symbol == stock["symbol"]:
-                    if float(shares) > (db.execute("SELECT quantity FROM portfolios WHERE symbol = ?", symbol))[0]["quantity"]:
+                    if int(shares) > (db.execute("SELECT quantity FROM portfolios WHERE symbol = ?", symbol))[0]["quantity"]:
                         return apology("You must enter a valid number of shares", 422)
 
             quote = lookup(symbol)
             id = session["user_id"]
             cash = db.execute("SELECT cash FROM users WHERE id = ?", id)
-            cost = float(shares) * float(quote["price"])
+            cost = int(shares) * float(quote["price"])
             db.execute("INSERT INTO transactions (user_id, type, symbol, quantity, price, cost) VALUES (?, ?, ?, ?, ?, ?)", id, 'SELL', symbol, shares, quote["price"], cost)
             remcash = float(cash[0]["cash"]) + cost
-            db.execute("UPDATE users SET cash = ? WHERE id = ?", remcash, id)
+            db.execute("UPDATE users SET cash = ? WHERE id = ?", 10000, id)
             for stock in portfolio:
                 if symbol == stock["symbol"]:
                     old = db.execute("SELECT quantity FROM portfolios WHERE user_id = ? AND symbol = ?", id, symbol)

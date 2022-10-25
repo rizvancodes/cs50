ce4ac15c22771c8ce390e5801d800f50568755e2
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-25 15:58:22 +0000

    ddd, Oct 10/25/2022, YYYY, 3:58 A +0

--- final/app.py
+++ final/app.py
@@ -1,1 +1,15 @@
+import os
 
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")

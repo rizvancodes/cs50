9bdb85fb13d7d5e7308917660394af70b56d5736
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-26 23:43:28 +0000

    ddd, Oct 10/26/2022, YYYY, 11:43 A +0

--- final/v1/app.py
+++ final/v1/app.py
@@ -79,10 +79,11 @@ def index():
 @app.route('/_calculate_extcert_grade')
 def process_data():
     selected_course = request.args.get('selected_class', type=str)
-    selected_units = {(request.args.get('unit1', type=str): (request.args.get('grade1', type=str),
+    selected_units = {(request.args.get('unit1', type=str): request.args.get('grade1', type=str),
                       (request.args.get('unit2', type=str): request.args.get('grade2', type=str),
-                      request.args.get('unit3', type=str): request.args.get('grade3', type=str
-                        
+                      request.args.get('unit3', type=str): request.args.get('grade3', type=str,
+                      request.args.get('unit4', type=str): request.args.get('grade4', type=str,
+
     # process the two selected values here and return the response; here we just create a dummy string
 
     return jsonify(random_text="You selected the car brand: {} and the model: {}.".format(selected_class, selected_entry))

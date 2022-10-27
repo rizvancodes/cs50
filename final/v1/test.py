1b08be48cd4e82aa1a4a89461b7dcb4baaa071a1
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-27 08:31:07 +0000

    ddd, Oct 10/27/2022, YYYY, 8:31 A +0

--- final/v1/test.py
+++ final/v1/test.py
@@ -6,5 +6,7 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///courses.db")
 
-unit_details = db.execute("SELECT type FROM UNITS WHERE title = ?", 'Principles and Applications of Applied Science I')
+name = 'Principles and Applications of Applied Science I'
+
+unit_details = db.execute("SELECT type FROM UNITS WHERE title = ?", name)
 print(unit_details)

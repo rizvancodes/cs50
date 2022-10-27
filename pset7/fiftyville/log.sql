659f16f7d9dc13b46f178ca20a5f8ca97ede5252
RizvanFarman <105713779+RizvanFarman@users.noreply.github.com>
2022-10-27 16:21:43 +0000

    ddd, Oct 10/27/2022, YYYY, 4:21 A +0

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -7,3 +7,5 @@
 SELECT description, street FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28;
 -- check interview schema
 .schema interviews
+-- get transcripts of interviews that mention word bakery
+SELECT transcript, year, month, day, name FROM interviews WHERE transcript LIKE '%bakery%';
